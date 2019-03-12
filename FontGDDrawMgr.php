08cfa538a379e26c4bd791d3cb0c80d748b3f0e8
MIKRI <2007mik007@mail.ru>
2019-03-12 16:54:52 +0700

    Added class FontGDDrawMgr, that control font settings and draw text on resource from GD library.
    Добавил класс FontGDDrawMgr, управляющий параметрами шрифта, и рисующий на переданном от GD библиотеки ресурсе изображения текст.

---
+++ FontGDDrawMgr.php
@@ -0,0 +1,174 @@
+﻿<?php
+
+/**
+ * Класс, управляющий отрисовкой текста на графике,
+ * используя библиотеку GD
+ * 
+ * @author Михаил Рыжков <2007mik007@mail.ru>
+ */
+
+namespace MIKRI\ChartMgr;
+
+class FontGDDrawMgr
+{
+    /**
+     * Путь к ttf файлу шрифта
+     * 
+     * @var string
+     */
+    private $ttfFontFilePath;
+
+    /**
+     * Размер шрифта
+     * 
+     * @var int
+     */
+    private $ttfFontSize;
+
+    /**
+     * Угол поворота текста
+     * 
+     * @var int
+     */
+    private $ttfAngle;
+
+    /**
+     * Массив из 3-х целочисленных элементов,
+     * содержищих цвет текста в формате RGB
+     * 
+     * @var array
+     */
+    private $ttfFontColor;
+
+    /**
+     * Текст последней ошибки
+     * 
+     * @var string
+     */
+    private $errorMsg;
+
+    /**
+     * Конструктор класса
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->ttfFontFilePath = "";
+        $this->errorMsg = "";
+    }
+
+    /**
+     * Получить последнюю ошибку
+     * 
+     * @return string
+     */
+    public function getLastError()
+    {
+        return $this->errorMsg;
+    }
+
+    /**
+     * Задать путь до ttf файла шрифта
+     * 
+     * @param string $filePath Путь к tff файлу шрифта
+     * 
+     * @return bool
+     */
+    public function setFontFilePath($filePath)
+    {
+        if (\file_exists($filePath)) {
+            $this->ttfFontFilePath = $filePath;
+            return true;
+        } else {
+            $this->errorMsg = "Файл " . $filePath . " не найден в системе";
+        }
+    }
+
+    /**
+     * Задаем параметры шрифта
+     * 
+     * @param int   $size  Размер шрифта, по умолчанию, 10
+     * @param array $color Массив из 3-х целочисленных элементов: R, G, B
+     * @param int   $angle Угол поворота текста, по умолчанинию, 0 - горизонтально
+     * 
+     * @return bool
+     */
+    public function setFontParams($size = 12, $color = array(0, 0, 0), $angle = 0)
+    {
+        $this->ttfFontSize = intval($size);
+        $this->ttfAngle = intval($angle);
+
+        // Проверяем заданыый цвет шрифта
+        if (\count($color) <> 3) {
+            $this->errorMsg = "Массив color должен состоять из 3-х целых чисел "
+                . "от 0 до 255, формирующих цвет в формате RGB";
+            return false;
+        }
+
+        foreach ($color as &$rgbEl) {
+            $rgbEl = intval($rgbEl);
+            if ($rgbEl < 0) {
+                $rgbEl = 0;
+            } else if ($rgbEl > 255) {
+                $rgbEl = 255;
+            }
+        }
+
+        $this->ttfFontColor = $color;
+    }
+
+    /**
+     * Рисует на переданном изображении переданный текст
+     * 
+     * @param resource $imgHandle Ресурс изображения от библиотеки GD
+     * @param int      $x         Координата оси X левого нижнего угла текста
+     * @param int      $y         Координата оси Y левого нижнего угла текста
+     * @param string   $text      Текст для отрисовки
+     * 
+     * @return bool
+     */
+    public function drawText(&$imgHandle, $x, $y, $text)
+    {
+        if ($imgHandle === false) {
+            $this->errorMsg = "Передан пустой ресурс изображения";
+            return false;
+        } else {
+            $fontColor = \imagecolorallocate(
+                $imgHandle,
+                $this->ttfFontColor[0],
+                $this->ttfFontColor[1],
+                $this->ttfFontColor[2]
+            );
+            if ($fontColor === false) {
+                $errorInf = error_get_last();
+                $this->errorMsg = "Ошибка при формировании цвета: ("
+                    . $errorInf['type'] . ") " . $errorInf['message']
+                    . " в файле " . $errorInf['file'] . " строка "
+                    . $errorInf['line'];
+                return false;
+            }
+
+            $textArea = \imagettftext(
+                $imgHandle,
+                $this->ttfFontSize,
+                $this->ttfAngle,
+                $x,
+                $y,
+                $fontColor,
+                $this->ttfFontFilePath,
+                $text
+            );
+            if ($textArea === false) {
+                $errorInf = error_get_last();
+                $this->errorMsg = "Ошибка при отрисовке текста: ("
+                    . $errorInf['type'] . ") " . $errorInf['message']
+                    . " в файле " . $errorInf['file'] . " строка "
+                    . $errorInf['line'];
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
